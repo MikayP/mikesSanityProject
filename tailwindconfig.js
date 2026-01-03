adbcdaa96f27821d27ac7a1069478b274a940d80
Mike Portman <mikeportmanj@gmail.com>
2026-01-02 20:38:40 -0600

    colors working

--- tailwindconfig.js
+++ tailwindconfig.js
@@ -1,15 +1,16 @@
-/** @type import('tailwindcss').Config */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-    theme: {
-    extend: {
-        colors:{
-            
-        }
-    },
-    },
-    plugins: [],
-}
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//     "./app/**/*.{js,ts,jsx,tsx}",
+//     "./styles/**/*.scss", // Add this so Tailwind scans your SCSS files
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         cobalt: "rgba(var(--cobalt), <alpha-value>)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
